d749e2c6e87d8237923d004a4356ea2a93fa64c8
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-05-18 19:56:51 -0300

    update entry module

--- project/index.ts
+++ project/index.ts
@@ -1,1 +1,21 @@
-console.log("Init");
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { stack } from "./src/inquirer";
+import handleJavaScript from "./src/stack/javascript";
+
+const commander = new Command();
+
+commander
+  .command("create <project-name>")
+  .description("Create a new project")
+  .action(async (projectName) => {
+    const stackName = await stack();
+
+    if (stackName === "JavaScript") {
+      // console.log(process.argv);
+      handleJavaScript(projectName);
+    }
+  });
+
+commander.parse(process.argv);
