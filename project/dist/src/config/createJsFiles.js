6202bc9db2da78d1a09607a950e73703cee089c5
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-05-20 17:21:17 -0300

    add creation of models, routes and controller folder

--- project/dist/src/config/createJsFiles.js
+++ project/dist/src/config/createJsFiles.js
@@ -1,27 +1,34 @@
 import fs from "fs/promises";
 async function createJsFiles(versions, projectName, addcors) {
     const files = new Map();
-    files.set("server.js", `const express = require("express");
-      const cors = require("cors");
-  
-  const app = express();
-  
-  app.use(cors())
-  
-  app.listen(3000, () => {
-      console.log("server started on port 3000");
-  });
+    const folders = ["routes", "models", "controllers"];
+    files.set("server.js", `import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import todoRoute from "./routes/todo";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use("/api", todoRoute);
+
+app.listen(3000, () => {
+    console.log("server started on port 3000");
+});
       `);
-    files.set(".env", "DB_URI=mongodb://localhost:27017");
+    files.set(".env", "DB_URI=mongodb://localhost:27017/todo");
     files.set(".gitignore", `node_modules
 .env
   `);
     addcors === "y" || addcors === "yes" ?
         files.set("package.json", `{
-    "name": "",
+    "name": "${projectName}",
     "version": "1.0.0",
     "description": "",
-    "type": "commonjs",
+    "type": "module",
     "scripts": {
         "dev": "node server.js"
     },
@@ -39,10 +46,10 @@ async function createJsFiles(versions, projectName, addcors) {
 }
   `) :
         files.set("package.json", `{
-    "name": "",
+    "name": "${projectName}",
     "version": "1.0.0",
     "description": "",
-    "type": "commonjs",
+    "type": "module",
     "scripts": {
         "dev": "node server.js"
     },
@@ -50,20 +57,54 @@ async function createJsFiles(versions, projectName, addcors) {
     "author": "",
     "license": "ISC",
     "dependencies": {
-      "dotenv": "^${versions[0]}",
-      "express": "^${versions[1]}",
-      "mongoose": "^${versions[2]}"
+        "dotenv": "^${versions[0]}",
+        "express": "^${versions[1]}",
+        "mongoose": "^${versions[2]}"
     }
 }
   `);
     try {
         await fs.mkdir(projectName);
+        for (let folder of folders) {
+            await fs.mkdir(`./${projectName}/${folder}`);
+            if (folder === "routes") {
+                await fs.writeFile(`./${projectName}/${folder}/todo.js`, `import { Router } from "express";
+import { todo } from "../controller/todo.js"
+        
+const router = Router();
+
+router.get("/todo", todo);
+
+export default router;
+        `);
+                continue;
+            }
+            if (folder === "controllers") {
+                await fs.writeFile(`./${projectName}/${folder}/todo.js`, `export function todo(req, res) {
+  res.json("Hello, world!");
+}
+        `);
+                continue;
+            }
+            if (folder === "models") {
+                await fs.writeFile(`./${projectName}/${folder}/todo.js`, `import { model, Schema } from "mongoose"
+const todoSchema = new Schema({
+  todo: {
+    type: String
+  }
+});
+
+export default model("todos", todoSchema);
+        `);
+            }
+        }
         for (let file of files) {
             await fs.writeFile(`./${projectName}/${file[0]}`, file[1]);
         }
     }
     catch (error) {
-        throw new Error(error.message);
+        const e = error;
+        throw new Error(`${e.message}, ${e.cause}`);
     }
 }
 export default createJsFiles;
--- project/src/config/createJsFiles.ts
+++ project/src/config/createJsFiles.ts
@@ -5,22 +5,30 @@ async function createJsFiles(versions: string[], projectName: string, addcors: s
 
   const files = new Map();
 
+  const folders = ["routes", "models", "controllers"];
+
   files.set(
       "server.js",
-      `const express = require("express");
-      const cors = require("cors");
-  
-  const app = express();
-  
-  app.use(cors())
-  
-  app.listen(3000, () => {
-      console.log("server started on port 3000");
-  });
+      `import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import todoRoute from "./routes/todo";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use("/api", todoRoute);
+
+app.listen(3000, () => {
+    console.log("server started on port 3000");
+});
       `
     );
     
-  files.set(".env", "DB_URI=mongodb://localhost:27017");
+  files.set(".env", "DB_URI=mongodb://localhost:27017/todo");
   
   files.set(
     ".gitignore",
@@ -34,10 +42,10 @@ async function createJsFiles(versions: string[], projectName: string, addcors: s
   files.set(
     "package.json",
     `{
-    "name": "",
+    "name": "${projectName}",
     "version": "1.0.0",
     "description": "",
-    "type": "commonjs",
+    "type": "module",
     "scripts": {
         "dev": "node server.js"
     },
@@ -59,10 +67,10 @@ async function createJsFiles(versions: string[], projectName: string, addcors: s
   files.set(
     "package.json",
     `{
-    "name": "",
+    "name": "${projectName}",
     "version": "1.0.0",
     "description": "",
-    "type": "commonjs",
+    "type": "module",
     "scripts": {
         "dev": "node server.js"
     },
@@ -70,23 +78,67 @@ async function createJsFiles(versions: string[], projectName: string, addcors: s
     "author": "",
     "license": "ISC",
     "dependencies": {
-      "dotenv": "^${versions[0]}",
-      "express": "^${versions[1]}",
-      "mongoose": "^${versions[2]}"
+        "dotenv": "^${versions[0]}",
+        "express": "^${versions[1]}",
+        "mongoose": "^${versions[2]}"
     }
 }
   `
-  );
+  )
 
   try {
     await fs.mkdir(projectName);
+
+    for (let folder of folders) {
+      await fs.mkdir(`./${projectName}/${folder}`);
+
+      if (folder === "routes") {
+        await fs.writeFile(`./${projectName}/${folder}/todo.js`, 
+        `import { Router } from "express";
+import { todo } from "../controller/todo.js"
+        
+const router = Router();
+
+router.get("/todo", todo);
+
+export default router;
+        `)
+
+        continue;
+      }
+
+      if (folder === "controllers") {
+        await fs.writeFile(`./${projectName}/${folder}/todo.js`, 
+        `export function todo(req, res) {
+  res.json("Hello, world!");
+}
+        `)
+
+        continue;
+      }
+
+      if (folder === "models") {
+        await fs.writeFile(`./${projectName}/${folder}/todo.js`, 
+        `import { model, Schema } from "mongoose"
+const todoSchema = new Schema({
+  todo: {
+    type: String
+  }
+});
+
+export default model("todos", todoSchema);
+        `)
+      }
+    }
 
     for (let file of files) {
       await fs.writeFile(`./${projectName}/${file[0]}`, file[1]);
     }
 
   } catch (error) {
-    throw new Error((error as Error).message);
+    const e = error as Error;
+
+    throw new Error(`${e.message}, ${e.cause}`);
   }
 }
 
