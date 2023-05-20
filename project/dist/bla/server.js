d2cd4f01c8c3fed6fd2a3d58b9e8a5880687e9eb
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-05-20 15:23:18 -0300

    update

--- project/dist/bla/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const express = require("express");
-const app = express();
-app.listen(3000, () => {
-    console.log("server started on port 3000");
-});
--- project/dist/index.js
+++ project/dist/index.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { stack } from "./src/inquirer";
-import handleJavaScript from "./src/stack/javascript";
+import { stack } from "./src/inquirer.js";
+import handleJavaScript from "./src/stack/javascript.js";
 const commander = new Command();
 commander
-    .command("create <project-name>")
-    .description("Create a new project")
-    .action(async (projectName) => {
+  .command("create <project-name>")
+  .description("Create a new project")
+  .action(async (projectName) => {
     const stackName = await stack();
     if (stackName === "JavaScript") {
-        // console.log(process.argv);
-        handleJavaScript(projectName);
+      // console.log(process.argv);
+      handleJavaScript(projectName);
     }
-});
+  });
 commander.parse(process.argv);
